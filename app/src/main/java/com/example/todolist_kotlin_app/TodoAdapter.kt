4340d2fef884db527d4947064bcc4fbc28ea98fb
TILLERN <brysonminodi@google.com>
2021-11-22 09:55:59 +0300

    rv methods

--- app/src/main/java/com/example/todolist_kotlin_app/TodoAdapter.kt
+++ app/src/main/java/com/example/todolist_kotlin_app/TodoAdapter.kt
@@ -1,63 +1,37 @@
 package com.example.todolist_kotlin_app
 
+import android.content.Context
 import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.icu.text.CaseMap
 import android.view.LayoutInflater
+import android.view.LayoutInflater.from
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.coroutines.coroutineContext
 
-class TodoAdapter(private val todos: MutableList<Todo>): RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
-    class TodoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+class TodoAdapter(private val context: Context): RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
+    private val layoutInflater = LayoutInflater.from(context)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        return TodoViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.addtodo,
-                parent,
-                false
-            )
-        )
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+        val myHeading = itemView?.findViewById<TextView?>(R.id.tvHeader)
+        val myDescription = itemView?.findViewById<TextView?>(R.id.tvDescription)
+
     }
 
-    fun addTodo(todo: Todo){
-        todos.add(todo)
-        notifyItemInserted(todos.size-1)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = layoutInflater.inflate(R.layout.list_item, parent, false)
+        return ViewHolder(itemView)
     }
 
-    fun deleteDoneTodo(){
-        todos.removeAll { todo ->
-            todo.isChecked
-        }
-        notifyDataSetChanged()
-    }
-
-    private fun toggleStrikeThrough(tvTodoTitle: TextView, isChecked: Boolean ){
-        if (isChecked){
-            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
-        } else {
-            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags.inv()
-        }
-    }
-
-    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        val currentTodo = todos[position]
-
-        holder.itemView.apply {
-            tvTodoTitle.text = currentTodo.title
-            cbDone.isChecked = currentTodo.isChecked
-            toggleStrikeThrough(tvTodoTitle, currentTodo.isChecked)
-            cbDone.setOnCheckedChangeListener{ - , isChecked ->
-                toggleStrikeThrough( tvTodoTitle, isChecked)
-                currentTodo.isChecked = !currentTodo.isChecked
-
-            }
-        }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
-        return todos.size
+        TODO("Not yet implemented")
     }
+
 }
